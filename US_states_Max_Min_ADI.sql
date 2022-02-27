d00ff0467113ee344d592814157cb4e8db3efe4c
ChauhanHrithik <100343727+ChauhanHrithik@users.noreply.github.com>
2022-02-27 22:51:28 +0530

    Delete US_states_Max_Min_ADI.sql

--- US_states_Max_Min_ADI.sql
+++
@@ -1,8 +0,0 @@
---U.S. states Maximum and Minimum ADI percentiles rankings overtime
-SELECT state
-,year
-,max(area_deprivation_index_percent) AS max_ADI
-,min(area_deprivation_index_percent) AS min_ADI
-FROM `bigquery-public-data.broadstreet_adi.area_deprivation_index_by_census_block_group`
-GROUP BY year
-,state
