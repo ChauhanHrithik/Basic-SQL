68cca9d481c56c8314ff79954663236e439f2f7d
ChauhanHrithik <100343727+ChauhanHrithik@users.noreply.github.com>
2022-02-28 15:05:32 +0530

    Update Asian&Black_rep.sql

--- Asian&Black_rep.sql
+++ Asian&Black_rep.sql
@@ -1,7 +1,7 @@
 -- Asian and Black race representation in Google YoY
-SELECT report_year
+select report_year
 , race_asian
 , race_black
- FROM `bigquery-public-data.google_dei.dar_non_intersectional_representation` 
+from `bigquery-public-data.google_dei.dar_non_intersectional_representation` 
 where workforce = 'representation_tech'
 order by report_year desc
