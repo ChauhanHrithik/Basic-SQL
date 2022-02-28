7205622f7fc0fe4a4b9552dc0df11a02becd1212
ChauhanHrithik <100343727+ChauhanHrithik@users.noreply.github.com>
2022-02-28 15:03:57 +0530

    Update Alcohol_Iowa.sql

--- Alcohol_Iowa.sql
+++ Alcohol_Iowa.sql
@@ -1,6 +1,6 @@
 --Most prefered alcohol in Iowa, US
-SELECT item_description
+select item_description
 ,sum(bottles_sold) as bottles_sold
-FROM `bigquery-public-data.iowa_liquor_sales.sales` 
+from `bigquery-public-data.iowa_liquor_sales.sales` 
 group by item_description
 order by bottles_sold desc 
