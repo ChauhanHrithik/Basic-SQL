25b4871646d71dd255c336d32f09c1a52a86dede
ChauhanHrithik <100343727+ChauhanHrithik@users.noreply.github.com>
2022-02-28 15:06:32 +0530

    Update Avg_price_room_Brazil.sql

--- Avg_price_room_Brazil.sql
+++ Avg_price_room_Brazil.sql
@@ -1,6 +1,6 @@
 --Average price of a room by neighborhood in Brazil
-SELECT place_name
+select place_name
 ,avg(price_aprox_usd) as avg_price 
-FROM `properati-data-public.properties_br.properties_rent_201712` 
+from `properati-data-public.properties_br.properties_rent_201712` 
 where rooms = 1
 group by place_name
