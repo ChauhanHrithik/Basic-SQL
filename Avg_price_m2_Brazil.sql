774f68ed2303e607641ef453c98d4da4c38bb2a5
ChauhanHrithik <100343727+ChauhanHrithik@users.noreply.github.com>
2022-02-28 15:06:08 +0530

    Update Avg_price_m2_Brazil.sql

--- Avg_price_m2_Brazil.sql
+++ Avg_price_m2_Brazil.sql
@@ -1,6 +1,6 @@
 --Average prices per square meter by neighborhood in Brazil
-SELECT place_name
+select place_name
 ,avg(price_usd_per_m2) as avg_price_m2
-FROM `properati-data-public.properties_br.properties_sell_201801` 
-GROUP BY place_name
+from `properati-data-public.properties_br.properties_sell_201801` 
+group by place_name
 order by avg_price_m2 desc
