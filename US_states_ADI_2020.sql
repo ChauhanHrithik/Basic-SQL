248539aed9391ecaaeee0da9d4e744d3dacc2140
ChauhanHrithik <100343727+ChauhanHrithik@users.noreply.github.com>
2022-02-28 15:07:28 +0530

    Update US_states_ADI_2020.sql

--- US_states_ADI_2020.sql
+++ US_states_ADI_2020.sql
@@ -1,11 +1,11 @@
 --U.S. states ADI percentiles rankings in 2020
-SELECT state_fips_code
+select state_fips_code
 ,state
 ,year
-,AVG(area_deprivation_index_percent) AS ADI
-FROM`bigquery-public-data.broadstreet_adi.area_deprivation_index_by_census_block_group`
+,AVG(area_deprivation_index_percent) as ADI
+from `bigquery-public-data.broadstreet_adi.area_deprivation_index_by_census_block_group`
 where year = 2020
-GROUP BY year
+group by year
 ,state_fips_code
 ,state
-ORDER BY state_fips_code
+order by state_fips_code
