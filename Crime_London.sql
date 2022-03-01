c1fa7cdc36e73e4006bd4d54ab838a47d89af42e
ChauhanHrithik <100343727+ChauhanHrithik@users.noreply.github.com>
2022-03-01 15:53:36 +0530

    Create Crime_London.sql

---
+++ Crime_London.sql
@@ -0,0 +1,9 @@
+--Top crimes per borough, London from 2011 to 2016
+select borough
+, major_category
+, sum(value) as Total_Incidents
+from  `bigquery-public-data.london_crime.crime_by_lsoa`
+group by borough
+, major_category 
+order by borough 
+,Total_Incidents desc
